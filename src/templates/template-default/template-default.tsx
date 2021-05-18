c16f522755b3fc6391e5582c9f5be466dacb4506
Khiemnd <khiemnd777@gmail.com>
2021-05-18 22:28:50 +0700

    template default

---
+++ src/templates/template-default/template-default.tsx
@@ -0,0 +1,18 @@
+import { FunctionalComponent, h } from 'preact';
+import { TemplateArgs } from '_stdio/core/template/template-interfaces';
+import Placeholder from '_stdio/core/placeholder/placeholder';
+import { TemplateFactory } from '_stdio/core/template/template-factory';
+import { BuildClassNameBind } from '_stdio/core/theme/theme-utils';
+
+const TemplateDefault: FunctionalComponent<TemplateArgs> = ({ theme, widgets, routerParams, visitorId }) => {
+  const cx = BuildClassNameBind(theme.Name, 'template_default');
+  return (
+    <div class={cx('template_default')}>
+      <div class={cx('body')}>
+        <Placeholder name={'body'} theme={theme} widgets={widgets} routerParams={routerParams} visitorId={visitorId} />
+      </div>
+    </div>
+  );
+};
+
+export default TemplateFactory.Register('template_default', TemplateDefault);
