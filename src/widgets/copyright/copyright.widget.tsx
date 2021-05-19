2eb2da0de038f1d0373570af086ffec5b2bcd72c
Khiemnd <khiemnd777@gmail.com>
2021-05-19 22:44:27 +0700

    feature/resume_refactor-copyright-and-theme-factory

--- src/widgets/copyright/copyright.widget.tsx
+++ src/widgets/copyright/copyright.widget.tsx
@@ -2,24 +2,18 @@ import { isEmpty } from 'lodash-es';
 import { Fragment, FunctionalComponent, h } from 'preact';
 import { BuildClassNameBind } from '_stdio/core/theme/theme-utils';
 import { WidgetFactory } from '_stdio/core/widget/widget-factory';
-import { MediaFormatEnums } from '_stdio/shared/enums/image-enums';
-import { GetSingleMedia } from '_stdio/shared/utils/media.utils';
 import { CopyrightWidgetArgs } from './copyright-interfaces';
 
 const CopyrightWidget: FunctionalComponent<CopyrightWidgetArgs> = ({ data, theme }) => {
   const cx = BuildClassNameBind(theme.Name, 'copyright');
-  const logo = GetSingleMedia(data?.Logo, MediaFormatEnums.ordinary);
   return (
     <div class={cx('copyright', !isEmpty(data) ? 'visible' : null)}>
       {!isEmpty(data) ? (
         <Fragment>
-          <div class={cx('logo')}>
-            <img src={logo.url} alt={data?.Logo?.Caption} />
-          </div>
           <div class={cx('copyright_container')}>
             {data.Copyright ? (
               <div class={cx('copyright_item')}>
-                <span>&copy;{data.Copyright}</span>
+                <span>&copy; {data.Copyright}</span>
               </div>
             ) : null}
             {data.PoweredBy ? (
