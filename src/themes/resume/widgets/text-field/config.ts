d1189f04f72d756befee23ada7536323db4167bf
DESKTOP-FAKMJLK\User <khiemnd777@gmail.com>
2021-05-22 16:14:15 +0700

    feature/resume_skill-tags-style

--- src/themes/resume/widgets/text-field/config.ts
+++ src/themes/resume/widgets/text-field/config.ts
@@ -1,4 +1,6 @@
 import { ThemeFactory } from '_stdio/core/theme/theme-factory';
 import styles from './text-field.styled.scss';
+import skillTagsStyles from './text-field.skill-tags.styled.scss';
 
 ThemeFactory.Register('resume', 'text_field', styles);
+ThemeFactory.Register('resume', 'text_field_skill_tags', skillTagsStyles);
