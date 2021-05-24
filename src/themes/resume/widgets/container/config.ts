cc05fea477c7951cac609877a70e08b37c3f265a
DESKTOP-FAKMJLK\User <khiemnd777@gmail.com>
2021-05-24 22:07:04 +0700

    feature/resume_improve-container-logo-copyright-mobile

--- src/themes/resume/widgets/container/config.ts
+++ src/themes/resume/widgets/container/config.ts
@@ -1,4 +1,6 @@
 import { ThemeFactory } from '_stdio/core/theme/theme-factory';
 import styles from './container.styled.scss';
+import containerOuterStyles from './container-outer.styled.scss';
 
 ThemeFactory.Register('resume', 'container', styles);
+ThemeFactory.Register('resume', 'container_outer', containerOuterStyles);
