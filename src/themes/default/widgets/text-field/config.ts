8b649f4b10de72b03efcb5b3469cd7d5971e80d7
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-07-06 00:54:07 +0700

    footer ui

--- src/themes/default/widgets/text-field/config.ts
+++ src/themes/default/widgets/text-field/config.ts
@@ -1,4 +1,6 @@
 import { ThemeFactory } from '_stdio/core/theme/theme-factory';
 import styles from './text-field.styled.scss';
+import footerStyles from './footer-text-field.styled.scss';
 
 ThemeFactory.Register('default', 'text_field', styles);
+ThemeFactory.Register('default', 'footer_text_field', footerStyles);
--- src/themes/default/widgets/title/config.ts
+++ src/themes/default/widgets/title/config.ts
@@ -1,4 +1,6 @@
 import { ThemeFactory } from '_stdio/core/theme/theme-factory';
 import styles from './title.styled.scss';
+import footerStyles from './footer-title.styled.scss';
 
 ThemeFactory.Register('default', 'title', styles);
+ThemeFactory.Register('default', 'footer_title', footerStyles);
