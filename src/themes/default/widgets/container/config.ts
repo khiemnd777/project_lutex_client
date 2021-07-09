048e4d5bb0267a8e4b9991104a5b405d0ae9f269
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-07-09 23:56:01 +0700

    adjust ui for template

--- src/themes/default/widgets/container/config.ts
+++ src/themes/default/widgets/container/config.ts
@@ -23,6 +23,9 @@ import footerCopyrightContainerStyles from './footer-copyright-container.styled.
 import introductionContainerStyles from './introduction-container.styled.scss';
 import introductioniImageContainerStyles from './introduction-image-container.styled.scss';
 import introductionTextContainerStyles from './introduction-text-container.styled.scss';
+// Top catalog container
+import topCatalogContainerStyles from './top-catalog-container.styled.scss';
+import topCatalogWrapperContainerStyles from './top-catalog-wrapper-container.styled.scss';
 
 // Default
 ThemeFactory.Register('default', 'container', styles);
@@ -49,3 +52,6 @@ ThemeFactory.Register('default', 'footer_copyright_container', footerCopyrightCo
 ThemeFactory.Register('default', 'introduction_container', introductionContainerStyles);
 ThemeFactory.Register('default', 'introduction_image_container', introductioniImageContainerStyles);
 ThemeFactory.Register('default', 'introduction_text_container', introductionTextContainerStyles);
+// Top catalog container
+ThemeFactory.Register('default', 'top_catalog_container', topCatalogContainerStyles);
+ThemeFactory.Register('default', 'top_catalog_wrapper_container', topCatalogWrapperContainerStyles);
