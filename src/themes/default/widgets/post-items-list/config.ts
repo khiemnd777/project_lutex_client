a640fba81021520879ca192761e4a3797fe2ff32
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-07-09 15:26:23 +0700

    common and single post ui

--- src/themes/default/widgets/post-items-list/config.ts
+++ src/themes/default/widgets/post-items-list/config.ts
@@ -1,4 +1,10 @@
 import { ThemeFactory } from '_stdio/core/theme/theme-factory';
 import style from './post-items-list.styled.scss';
+import commonStyles from './common.post-items-list.styled.scss';
+import commonSmallStyles from './common-small.post-items-list.styled.scss';
+import singleStyles from './single.post-items-list.styled.scss';
 
 ThemeFactory.Register('default', 'post_items_list', style);
+ThemeFactory.Register('default', 'post_items_list_common', commonStyles);
+ThemeFactory.Register('default', 'post_items_list_common_small', commonSmallStyles);
+ThemeFactory.Register('default', 'post_items_list_single', singleStyles);
