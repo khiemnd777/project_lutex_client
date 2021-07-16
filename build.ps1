75001264f788d34ec2e5efea15159576ca683dad
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-07-16 14:39:06 +0700

    change build path

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 # Invoke cake build
-.\src\_stdio\devops\cake\build.ps1 @args
+& ($PSScriptRoot + "\src\_stdio\devops\cake\build.ps1") @args
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 Set-Location -LiteralPath $PSScriptRoot
