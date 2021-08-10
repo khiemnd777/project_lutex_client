dcc71b396ea644b84c419d600958a4814c94ccea
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-08-10 23:59:58 +0700

    featured post common style

--- src/themes/default/widgets/featured-posts/config.ts
+++ src/themes/default/widgets/featured-posts/config.ts
@@ -1,6 +1,8 @@
 import { ThemeFactory } from '_stdio/core/theme/theme-factory';
 import styles from './featured-posts.styled.scss';
 import simpleStyles from './simple.featured-posts.styled.scss';
+import commonSmallStyles from './common-small.featured-items-list.styled.scss';
 
 ThemeFactory.Register('default', 'featured_posts', styles);
 ThemeFactory.Register('default', 'featured_posts_simple', simpleStyles);
+ThemeFactory.Register('default', 'featured_posts_list_common_small', commonSmallStyles);
